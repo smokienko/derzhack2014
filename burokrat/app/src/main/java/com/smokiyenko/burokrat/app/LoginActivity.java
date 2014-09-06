fcd0dec9443be4fb1d2f3aa0060abd8685900298
mokiyenk <Sergiy.Mokiyenko@tomtom.com>
2014-09-06 13:20:25 +0300

    Added LoginActivityStart. Added LoginActivity Label.

--- burokrat/app/src/main/java/com/smokiyenko/burokrat/app/LoginActivity.java
+++ burokrat/app/src/main/java/com/smokiyenko/burokrat/app/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.smokiyenko.burokrat.app;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
--- burokrat/app/src/main/java/com/smokiyenko/burokrat/app/SplashScreen.java
+++ burokrat/app/src/main/java/com/smokiyenko/burokrat/app/SplashScreen.java
@@ -1,9 +1,12 @@
 package com.smokiyenko.burokrat.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
+
 
 
 public class SplashScreen extends Activity {
@@ -12,6 +15,15 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Start Login Activity
+        final Handler handler = new Handler();
+        final Runnable activityStartRunnable = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this,LoginActivity.class));
+            }
+        } ;
+        handler.postDelayed(activityStartRunnable,3000);
     }
 
 }
